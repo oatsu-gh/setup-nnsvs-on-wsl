5f4b5452ff04dd4a8b3822d163f15e90b9e59a14
unknown <panchi.psp@gmail.com>
2020-05-27 00:02:01 +0900

    バグのもとになりそうな文字列を削除

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,15 +3,13 @@ cd ~
 sudo apt update && sudo apt upgrade -y && sudo autoremove
 
 # pip3のインストール
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 
 # .bashrc に書き込み
 echo \ >> ~/.bashrc
-echo '# ここから追記'
+echo '# ここから追記' >> ~/.bashrc
 echo alias python=\'python3\' >> ~/.bashrc
 echo alias pip=\'pip3\' >> ~/.bashrc
-echo alias Hachune=\'Y(๑°口°๑)\'
-
 
 # 終了
 exit
